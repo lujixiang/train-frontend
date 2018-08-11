a35cb24acbc01aae83b1f57402e9e427287582a1
just be free <justbefree@126.com>
2018-08-11 14:51:13 +0800

    删除无用组件

--- src/components/passenger-v3/index.js
+++
@@ -1,6 +0,0 @@
-import passengers from './passengers.vue'
-
-passengers.install = (Vue) => {
-  Vue.component('passengerV3', passengers)
-}
-export default passengers
--- src/components/passenger/index.js
+++
@@ -1,7 +0,0 @@
-import passenger from './passenger.vue'
-
-passenger.install = (Vue) => {
-  Vue.component('passenger', passenger)
-}
-
-export default passenger
