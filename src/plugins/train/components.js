836e66a1b02f2cccc32bd0e72c64964a6f6642ba
just be free <justbefree@126.com>
2019-03-14 10:50:45 +0800

    调整项目目录结构

---
+++ src/plugins/train/components.js
@@ -0,0 +1,86 @@
+import ykbIcon from './icon'
+import filterBar from './filter-bar'
+import calendar from './calendar'
+import actionSheetHeader from './action-sheet-header'
+import datePickerPreviousNext from './datepicker-next-previous'
+import dateSelectCell from './date-select-cell'
+import citySelect from './city-station-cell'
+import searchHistory from './search-history'
+import bottom from './bottom'
+import banner from './banner'
+// 引入加强版的header
+import enhanceHeader from './enhance-header'
+import indexList from './index-list'
+import NSR from './no-search-result'
+import passengerV2 from './passenger-v2'
+import passengerRoundTrip from './passenger-v2-round-trip'
+import ticketCard from './ticket-card'
+import orderNotice from './order-notice'
+import timeList from './time-list'
+import stationInfo from './station-info'
+import seating from './seating'
+import cityPicker from './city-picker'
+import ykbCell from './cell'
+import search from './search'
+import noticeBar from './notice-bar'
+import keyboard from './keyboard'
+import editId from './edit-id'
+import onlineSeatSelection from './online-seat-selection'
+import onlineSeat from './online-seat'
+import textAlert from './text-alert'
+import tripCard from './trip-card'
+import navBar from './nav-bar'
+import tripTip from './trip-tips'
+import tripPicker from './trip-picker'
+import guidInstruction from './guid-instruction'
+import enhancePassengers from './enhance-passengers'
+import idWallet from './id-wallet'
+import idCard from './id-card'
+import idModify from './id-modify'
+import listView from './list-view'
+import outerList from './outer-list'
+import trainRules from './train-rules'
+const components = [
+  ykbIcon,
+  filterBar,
+  calendar,
+  actionSheetHeader,
+  datePickerPreviousNext,
+  dateSelectCell,
+  citySelect,
+  searchHistory,
+  bottom,
+  banner,
+  enhanceHeader,
+  indexList,
+  NSR,
+  passengerV2,
+  passengerRoundTrip,
+  ticketCard,
+  orderNotice,
+  timeList,
+  stationInfo,
+  seating,
+  cityPicker,
+  ykbCell,
+  search,
+  noticeBar,
+  keyboard,
+  editId,
+  onlineSeatSelection,
+  onlineSeat,
+  textAlert,
+  tripCard,
+  navBar,
+  tripTip,
+  tripPicker,
+  guidInstruction,
+  enhancePassengers,
+  idWallet,
+  idCard,
+  idModify,
+  listView,
+  outerList,
+  trainRules
+]
+export default components
