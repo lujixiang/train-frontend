35246594f773167e6d3d3aa7fd4edda7502728ac
wangxiaofang <wangxiaofang06@163.com>
2019-03-21 19:20:36 +0800

    no message

--- src/mixins/collect-data.js
+++ src/mixins/collect-data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { mapActions } from 'vuex'
 const moment = require('moment')
 export const collectUserAction = {
@@ -30,36 +29,3 @@ export const collectUserAction = {
     }
   }
 }
-=======
-import { mapActions } from 'vuex'
-const moment = require('moment')
-export const collectUserAction = {
-  data () {
-    return {}
-  },
-  methods: {
-    ...mapActions('history', ['recordUserOpeartion']),
-    collectUserData (args = {}, msg = {}) {
-      let { action } = args
-      return new Promise((resolve, reject) => {
-        let user = this.$store.state.company.currentUser
-        let params = {
-          time: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-          bline: 'train',
-          userid: JSON.stringify(user), // 用户系统库id
-          action: '', // 用户操作方式，
-          msg: JSON.stringify({...msg}),
-          ...args
-        }
-        this.recordUserOpeartion({resolve, reject, params})
-      })
-      .then(res => {
-        console.log(res, 'Successfully loged ', action)
-      })
-      .catch(err => {
-        console.log(err, 'log failed ', action)
-      })
-    }
-  }
-}
->>>>>>> dev
